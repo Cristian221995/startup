f9a18ba104ce2c9530558e9fb32191ec2d723592
Cristian Fonteina <39927364+Cristian221995@users.noreply.github.com>
2018-10-18 14:03:42 -0300

    Exercice 3.

--- 01-JS-DOM-APIs/js/script.js
+++ 01-JS-DOM-APIs/js/script.js
@@ -1,7 +1,7 @@
 function fadeInOnLoad(){  //fade in "Hello World" on load.
     setInterval( function() {
       let element = document.getElementById("hide");
-      element.classList.remove("hidden");
+      element.classList.add("hidden");
     },500);
     
   }
@@ -10,3 +10,58 @@ function fadeInOnLoad(){  //fade in "Hello World" on load.
     alert("Warning");
   }
 
+  function getData() {    //Get random joke form API
+    var xhr = new XMLHttpRequest();
+  
+    xhr.onreadystatechange = function() {
+      if(xhr.readyState == 4 && xhr.status==200)
+      {
+        console.log("received the response", xhr.responseText);
+        let response = JSON.parse(xhr.response);
+        document.getElementById("joke").innerHTML = response.value.joke;
+      } else {
+          console.log("error in processing the request");
+      }
+    }
+    xhr.open("GET","http://api.icndb.com/jokes/random",true);
+    xhr.send();
+  }
+  
+  //  info source = https://medium.com/front-end-hacking/ajax-async-callback-promise-e98f8074ebd7
+  function getJokePromise(){   //Get random joke from API
+    var URL = "http://api.icndb.com/jokes/random"
+    makeAjaxCall(URL,"GET").then(renderJoke,errorHandler);
+  }
+  
+  function renderJoke(jokeData){  //Send joke data to html
+    document.getElementById("joke").innerHTML = jokeData.value.joke;
+  }
+  
+  function makeAjaxCall(url, methodType){   // Reusable function to perform AJAX calls.
+     let promiseObj = new Promise(function(resolve, reject){
+        let xhr = new XMLHttpRequest();
+        xhr.open(methodType, url, true);
+        xhr.send();
+        xhr.onreadystatechange = function(){
+        if (xhr.readyState === 4){
+           if (xhr.status === 200){
+              console.log("xhr done successfully");
+              let resp = xhr.responseText;
+              let respJson = JSON.parse(resp);
+              resolve(respJson);
+           } else {
+              reject(xhr.status);
+              console.log("xhr failed");
+           }
+        } else {
+           console.log("xhr processing going on");
+        }
+     }
+     console.log("request sent succesfully");
+   });
+   return promiseObj;
+  }
+  
+  function errorHandler(statusCode){   //Show  'jokeContainer' section background in red when a server error occurs.
+    document.getElementById('jokeContainer').element.style.backgroundColor = "red";
+  }
