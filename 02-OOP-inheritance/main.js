8afb8f07aa3ff1ee3ea59c64cb28cdaf69139de7
Cristian221995 <Cristian221995@users.noreply.github.com>
2018-10-24 21:56:01 -0300

    ex 3

--- 02-OOP-inheritance/main.js
+++ 02-OOP-inheritance/main.js
@@ -3,7 +3,8 @@ class Movie extends EventEmmiter {
         super();
         this.title = title,
         this.year = year,
-        this.duration = duration
+        this.duration = duration,
+        this.cast = [];
     }
 
     play(){
@@ -18,9 +19,18 @@ class Movie extends EventEmmiter {
         this.emit('resume');
     }
 
+    addCast(actor){ //src: https://stackoverflow.com/questions/4775722/check-if-object-is-array
+        if(Array.isArray(actor)) //check if is an array of actors
+        {
+          this.cast = this.cast.concat(actor);
+        }
+        else{
+          this.cast.push(actor);
+        }
+    }   
 }
-
-var movie1 = new Movie("The Dark Night", 2012, "2hs");
+    
+var theDarkNight = new Movie("The Dark Night", 2012, "2hs");
 
 theDarkNight.play;
 
@@ -29,11 +39,11 @@ class Actor{
       this.name = name;
       this.age = age;
     }
-  };
+};
 
-  var actor1 = new Actor("Christian Bale", 30);
+var actor1 = new Actor("Christian Bale", 30);
 
-  class EventEmmiter {
+class EventEmmiter {
     constructor(){
       this.events = {};
     }
@@ -60,7 +70,24 @@ class Actor{
             this.events[eventName].splice(i, 1);
             break;
           }
-        };
+        }
       }
     }
+}
+
+
+
+class Logger{
+    constructor(){
     }
+
+    log(info){
+      console.log('The '+ info +' event has been emited');
+    }
+}
+
+var logger = new Logger();
+theDarkNight.on('play',logger.log);
+
+theDarkNight.addCast(actor1);
+theDarkNight.cast;
